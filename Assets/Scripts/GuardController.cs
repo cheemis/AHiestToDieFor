408d832af6f59ed10029f7f15d9e0ba2eb45e1be
cheemis <nnolasco@calpoly.edu>
2020-02-10 20:30:28 -0800

    imported prototype

---
+++ Assets/Scripts/GuardController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class GuardController : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    public Vector3[] patrolPoints;
+    public GameObject player;
+    private int currentPoint = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        FindNewPoint();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Vector3.Distance(transform.position,patrolPoints[currentPoint]) < .1)
+        {
+            Debug.Log(currentPoint);
+            FindNewPoint();
+        }
+    }
+
+    private void FindNewPoint()
+    {
+        currentPoint ++;
+        agent.SetDestination(patrolPoints[currentPoint % patrolPoints.Length]);
+    }
+
+    // private bool findPlayer()
+    // {
+    //     RaycastHit hit;
+    //     Ray ray = new Ray(viewPoint.transform.position, player.transform.position);
+    //     //float distance = (viewPoint.transform.position - player.transform.position).magnitude;
+
+    //     if(Physics.Raycast(ray, out hit))
+    //     {
+    //         if(hit.transform.gameObject.tag == "Player")
+    //         {
+    //             Debug.Log("found player");
+    //             return true;
+    //         }
+    //         Debug.Log("can't find player: " + hit.transform.gameObject);
+    //     }
+    //     Debug.Log(hit);
+    //     return false;
+    // }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.EventSystems;
+
+public class PlayerController : MonoBehaviour
+{
+    //public bool alive = true;
+    //public Camera camera;
+    private NavMeshAgent agent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePlayer();
+    }
+
+    private void MovePlayer()
+    {
+        if(Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject())
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+            Debug.Log("hit");
+            Vector3 location = hit.point;
+            agent.SetDestination(location);
+            }
+        }
+    }
+}
