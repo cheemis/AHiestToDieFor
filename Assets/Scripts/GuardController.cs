757555d98dcd2865547f9d39109dacd3ceb65be8
cheemis <nnolasco@calpoly.edu>
2020-02-13 09:59:48 -0800

    added comments

--- Assets/Scripts/GuardController.cs
+++ Assets/Scripts/GuardController.cs
@@ -20,6 +20,7 @@ public class GuardController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Look for the player, otherwise, patrol
         findPlayer();
         if(Vector3.Distance(transform.position, patrolPoints[currentPoint % patrolPoints.Length]) < .1)
         {
@@ -29,25 +30,35 @@ public class GuardController : MonoBehaviour
 
     private void FindNewPoint()
     {
+        //This function sets a new destination fot the guard
         currentPoint ++;
-        agent.SetDestination(patrolPoints[currentPoint % patrolPoints.Length]);
+        agent.SetDestination(patrolPoints[currentPoint % (patrolPoints.Length - 1)]);
     }
 
     private bool findPlayer()
     {
+        //Create Raycast
         RaycastHit hit;
         Ray ray = new Ray(viewPoint.transform.position, player.transform.position);
+        
+        //Visualize raycast in Scene play. Doesn't effect gameplay
         Debug.DrawRay(viewPoint.transform.position,(player.transform.position - viewPoint.transform.position), Color.white, 0.0f, true);
-        //float distance = (viewPoint.transform.position - player.transform.position).magnitude;
 
+        //see if the raycast hits something
         if(Physics.Raycast(viewPoint.transform.position, (player.transform.position - viewPoint.transform.position), out hit))
         {
+            //if the object is tagged the player
             if(hit.transform.gameObject.tag == "Player")
             {
+                //replace with chase the player
                 Debug.Log("found player");
                 return true;
             }
-            Debug.Log("can't find player: " + hit.transform.gameObject);
+            else
+            {
+                //replace with can't find player
+                Debug.Log("can't find player: " + hit.transform.gameObject);
+            }
         }
         Debug.Log(hit);
         return false;
