44f326821f9be4200ade99a2e162278934aa4f48
ejzien1109 <ejzien1109@gmail.com>
2020-02-26 15:00:48 -0800

    Respawns another robber behind the van when one dies

--- Assets/Scripts/Managers/SelectedManager.cs
+++ Assets/Scripts/Managers/SelectedManager.cs
@@ -11,6 +11,10 @@ public class SelectedManager : MonoBehaviour
     private List<Selected> selectedRobbers;
 
     public List<GameObject> presetRobbers;
+
+    public List<GameObject> availableRobbers;
+
+    private Vector3 respawnPosition = new Vector3(-34, 1, 6);
     private void Awake()
     {
         List<MonoBehaviour> deps = new List<MonoBehaviour>
@@ -53,6 +57,7 @@ public class SelectedManager : MonoBehaviour
     {
         presetRobbers = presetRobbers.Where(go => go != target).ToList();
         selectedRobbers = selectedRobbers.Where(sel => sel.go != target).ToList();
+        RespawnRobber(availableRobbers);
     }
     private void MoveSelectedRobbers(GameObject target, List<object> parameters)
     {
@@ -118,6 +123,16 @@ public class SelectedManager : MonoBehaviour
         if (selectedRobbers.Count != 0)
         {
             gem.TriggerEvent("UpdateCamera", robbers[0]);
+        }
+    }
+
+    private void RespawnRobber(List<GameObject> robbers)
+    {
+        if (robbers.Count > 0)
+        {
+            robbers[0].transform.position = respawnPosition;
+            presetRobbers.Add(robbers[0]);
+            robbers.RemoveAt(0);
         }
     }
 
