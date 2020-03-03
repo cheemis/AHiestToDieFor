bd3a1f3e9644bd49a0c1e68d8b33bea3729f8d21
cheemis <nnolasco@calpoly.edu>
2020-03-03 01:36:50 -0800

    fixed vault location

--- Assets/Scripts/RobberScripts/Unlocking.cs
+++ Assets/Scripts/RobberScripts/Unlocking.cs
@@ -54,6 +54,6 @@ public class Unlocking : MonoBehaviour
             return;
         }
 
-        gem.TriggerEvent("StopUnlocking", gameObject);
+        //gem.TriggerEvent("StopUnlocking", gameObject);
     }
 }
