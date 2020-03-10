59ab8074304f894e17b7c1dcfafa3d76e8762cef
Rafael Dolfe <rafael.dolfe@hotmail.com>
2020-03-10 09:04:17 -0700

    Fix minor inconvenience where the Sky mat is changed directly in script

--- Assets/Scripts/Sunrise.cs
+++ Assets/Scripts/Sunrise.cs
@@ -7,13 +7,15 @@ public class Sunrise : MonoBehaviour
     private float duration = 300f;
     public float timePassed = 0f;
     public Material mat;
+    public MeshRenderer meshr;
 
     private Color night = new Color32(68, 1, 141, 255);
     private Color day = new Color32(253, 184, 93, 255);
 
     private void Start()
     {
-        mat.color = night;
+        meshr = GetComponent<MeshRenderer>();
+        meshr.material.color = night;
     }
 
     // Update is called once per frame
@@ -22,11 +24,13 @@ public class Sunrise : MonoBehaviour
         ColorShift();
     }
 
+
+
     private void ColorShift()
     {
         if (this.tag == "Sky")
         {
-            mat.color = Color.Lerp(night, day, timePassed);
+            meshr.material.color = Color.Lerp(night, day, timePassed);
 
             if (timePassed < 1)
             {
