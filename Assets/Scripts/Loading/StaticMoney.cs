1e385ee57cdadcec9e10de9efb797520942de292
cheemis <nnolasco@calpoly.edu>
2020-03-10 20:42:19 -0700

    fixed money on restart

--- Assets/Scripts/Loading/StaticMoney.cs
+++ Assets/Scripts/Loading/StaticMoney.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class StaticMoney : MonoBehaviour
 {
 
+    private static float TotalMoney = 5000;
+
     private static float moneyCount = 5000;
 
     private static int robbersAlive = 0;
 
     private static int lastScene = 0;
 
+    public static float GetTotalMoneyCount() {return moneyCount;}
     public static float GetMoneyCount() {return moneyCount;}
     public static int GetRobbersAlive() {return robbersAlive;}
     public static void AddMoney(float money) {
@@ -23,6 +26,10 @@ public class StaticMoney : MonoBehaviour
     {
         moneyCount = money;
     }
+
+    public static void AddTotalMoney(float money) {TotalMoney += money;}
+
+    public static void ResetMoney() {moneyCount = TotalMoney;}
     public static void SetRobbersAlive(int robbers) {robbersAlive = robbers;}
     public static int GetLastScene() {return lastScene;}
     public static void SetLastScene(int scene) {lastScene = scene;}
