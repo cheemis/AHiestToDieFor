17600d467d1832e87d069dcc0b14ffe61782acef
cheemis <nnolasco@calpoly.edu>
2020-03-17 17:01:02 -0700

    made cutscene scene

---
+++ Assets/CutScene.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CutScene : MonoBehaviour
+{
+    public Camera camera;
+    public GameObject firstScene;
+    public GameObject secondScene;
+    public GameObject thirdScene;
+
+    private Vector3 nextPoint = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(nextPoint != Vector3.zero)
+        {
+            Vector3 slerped = Vector3.Lerp(camera.transform.position, nextPoint, Time.deltaTime * 2);
+            camera.transform.position = slerped;
+            if(slerped.magnitude < .1f) {nextPoint = Vector3.zero;}
+        }
+    }
+
+    public void LoadSecond()
+    {
+        secondScene.SetActive(true);
+        nextPoint = new Vector3(-56.32609f, -14.5f, -2.6f);
+        firstScene.SetActive(false);
+    }
+
+    public void LoadThird()
+    {
+        secondScene.SetActive(false);
+        nextPoint = new Vector3(-107.02f, -12.5f, 14.68f);
+        thirdScene.SetActive(true);
+    }
+
+    public void LoadLevel()
+    {
+        SceneManager.LoadSceneAsync(3);
+    }
+}
---
+++ Assets/Scripts/Cutscene/WalkingClones.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WalkingClones : MonoBehaviour
+{
+    public float speed = 1;
+    private float spawn = -90;
+    private float bound = -117.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.x > bound)
+        {
+            transform.Translate(0, 0, speed * Time.deltaTime);
+        }
+        else
+        {
+            transform.position = new Vector3(spawn, transform.position.y, transform.position.z);
+        }
+    }
+}
